7634f40a82d1bac0eea5aa9f26528b493251cc83
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-02 22:50:02 +0300

    Deploy

--- _app/immutable/chunks/paths.DNif_je7.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_tij0b)==null?void 0:s.base)??"/casatrade";var a;const e=((a=globalThis.__sveltekit_tij0b)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/entry/start.DM87EBas.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.yLtjyd8I.js";export{t as start};
